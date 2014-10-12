81cac04b536adb31d94ce30aa944d83c26e74060
appcontainers <rnason@formatccolon.com>
2014-10-11 22:30:21 -0400

    Update dockerup.sh

--- dockerup.sh
+++ dockerup.sh
@@ -1,4 +1,8 @@
- #!/bin/bash
+#!/bin/bash
+
+# SET Dockerup Variable Names to be used throughout the script
+App="wordpress"
+
 
 # Set Colors we will use for different prompts
 
@@ -25,7 +29,7 @@ NTerm='\e[0m'
 echo -e "\n"
 
 # Message to user
-echo -e "${BYellowTerm} Wordpress Container Configuration Script ${NTerm}"
+echo -e "${BYellowTerm} \"$APP\" Container Configuration Script ${NTerm}"
 echo -e "${BGreenTerm} You can hit enter on any of the following fields to accept the default values. ${NTerm}"
 
 # Set variable for the while loop
@@ -34,63 +38,63 @@ LOOP="true"
 # While the LOOP variable is true, loop through the questions below. This will allow for a re-think
 while [ "$LOOP" = "true" ] 
 do
-        echo -e "${GreyTerm} Wordpress instance domain name (without the www) {example.com} : ${RedTerm} [default:wordpress.local] ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" instance domain name (without the www) {example.com} : ${RedTerm} [default:\"$APP\".local] ${NTerm}"
         read -e FQDN
-		if [ "$FQDN" = "" ]; then
-                        FQDN="wordpress.local"
+                if [ "$FQDN" = "" ]; then
+                        FQDN="\"$APP\".local"
                 fi
         echo -e "\n"
 
-	echo -e "${GreyTerm} Root MySQL password: ${RedTerm} [default:P@ssw0rd] ${NTerm}"
+        echo -e "${GreyTerm} Root MySQL password: ${RedTerm} [default:P@ssw0rd] ${NTerm}"
         read -e MYSQLPASS
-        	if [ "$MYSQLPASS" = "" ]; then
+                if [ "$MYSQLPASS" = "" ]; then
                         MYSQLPASS="P@ssw0rd"
                 fi
-	echo -e "\n"
-	
-	echo -e "${GreyTerm} Database Server host name or IP address where the Wordpress database will reside: ${RedTerm} [default:localhost] ${NTerm}"
+        echo -e "\n"
+        
+        echo -e "${GreyTerm} Database Server host name or IP address where the \"$APP\" database will reside: ${RedTerm} [default:localhost] ${NTerm}"
         read -e DBHOST
                 if [ "$DBHOST" = "" ]; then
                         DBHOST="localhost"
                 fi
         echo -e "\n"
 
-	echo -e "${GreyTerm} MySQL Wordpress database name: ${RedTerm} [default:wordpress] ${NTerm}"
+        echo -e "${GreyTerm} MySQL \"$APP\" database name: ${RedTerm} [default:\"$APP\"] ${NTerm}"
         read -e DATABASE
-		if [ "$DATABASE" = "" ]; then
-			DATABASE="wordpress"
-		fi
+                if [ "$DATABASE" = "" ]; then
+                        DATABASE="\"$APP\""
+                fi
         echo -e "\n"
 
-        echo -e "${GreyTerm} MySQL Username that Wordpress will use to connect to the Wordpress database: ${RedTerm} [default:admin] ${NTerm}"
+        echo -e "${GreyTerm} MySQL Username that \"$APP\" will use to connect to the \"$APP\" database: ${RedTerm} [default:admin] ${NTerm}"
         read -e USER
-        	if [ "$USER" = "" ]; then
+                if [ "$USER" = "" ]; then
                         USER="admin"
                 fi
-	echo -e "\n"
+        echo -e "\n"
 
-	echo -e "${GreyTerm} Password for the new MySQL Wordpress database user: ${RedTerm} [default:P@ssw0rd] ${NTerm}"
+        echo -e "${GreyTerm} Password for the new MySQL \"$APP\" database user: ${RedTerm} [default:P@ssw0rd] ${NTerm}"
         read -e USERPASS
-        	if [ "$USERPASS" = "" ]; then
+                if [ "$USERPASS" = "" ]; then
                         USERPASS="P@ssw0rd"
                 fi
-	echo -e "\n"
+        echo -e "\n"
 
-	echo -e "${GreyTerm} Phrase to be used for Wordpress auth cookies: ${RedTerm} [default:Check...] ${NTerm}"
+        echo -e "${GreyTerm} Phrase to be used for \"$APP\" auth cookies: ${RedTerm} [default:Check...] ${NTerm}"
         read -e WPKEY
-		if [ "$WPKEY" = "" ]; then
+                if [ "$WPKEY" = "" ]; then
                         WPKEY="Check us out at www.appcontainers.com"
                 fi
         echo -e "\n"
 
-	echo -e "${RedTerm} Container Name (specified at docker run time): ${RedTerm} [default:wordpress] ${NTerm}"
+        echo -e "${RedTerm} Container Name (specified at docker run time): ${RedTerm} [default:\"$APP\"] ${NTerm}"
         read -e RUNNAME
-		if [ "$RUNNAME" = "" ]; then
-                        RUNNAME="wordpress"
+                if [ "$RUNNAME" = "" ]; then
+                        RUNNAME="\"$APP\""
                 fi
         echo -e "\n"
 
-	echo -e "${GreyTerm} Wordpress container Host TCP Port: ${RedTerm} [default:80] ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" container Host TCP Port: ${RedTerm} [default:80] ${NTerm}"
         read -e PORT
                 if [ "$PORT" = "" ]; then
                         PORT="80"
@@ -99,14 +103,14 @@ do
 
         echo -e "${BYellowTerm} You entered the following information: ${Nterm}"
 
-	echo -e "${GreyTerm} MySQL Host:                       ${GreenTerm} \"$DBHOST\" ${NTerm}"
-	echo -e "${GreyTerm} MySQL Root Password:              ${GreenTerm} \"$MYSQLPASS\" ${NTerm}"
-        echo -e "${GreyTerm} Wordpress Database Name:          ${GreenTerm} \"$DATABASE\" ${NTerm}"
-	echo -e "${GreyTerm} Wordpress User:                   ${GreenTerm} \"$USER\" ${NTerm}"
-	echo -e "${GreyTerm} Wordpress User Password:          ${GreenTerm} \"$USERPASS\" ${NTerm}"
-	echo -e "${GreyTerm} Wordpress Cookie Auth Phrase:     ${GreenTerm} \"$WPKEY\" ${NTerm}"
-	echo -e "${GreyTerm} Wordpress Container Name:         ${GreenTerm} \"$RUNNAME\" ${NTerm}"
-	echo -e "${GreyTerm} Wordpress Container Port:         ${GreenTerm} \"$PORT\" ${NTerm}"	
+        echo -e "${GreyTerm} MySQL Host:                       ${GreenTerm} \"$DBHOST\" ${NTerm}"
+        echo -e "${GreyTerm} MySQL Root Password:              ${GreenTerm} \"$MYSQLPASS\" ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" Database Name:           ${GreenTerm} \"$DATABASE\" ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" User:                    ${GreenTerm} \"$USER\" ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" User Password:           ${GreenTerm} \"$USERPASS\" ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" Cookie Auth Phrase:      ${GreenTerm} \"$WPKEY\" ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" Container Name:          ${GreenTerm} \"$RUNNAME\" ${NTerm}"
+        echo -e "${GreyTerm} \"$APP\" Container Port:          ${GreenTerm} \"$PORT\" ${NTerm}" 
 
         echo -e "${BRedTerm} Is this Information correct? (y/n) ${NTerm}"
         read -e CONFIRM
@@ -117,9 +121,25 @@ do
         fi
 done
 
-echo -e "${BYellowTerm} Starting Wordpress Container... ${NTerm}"
+echo -e "${BRedTerm} Press [Enter] to create the \"$APP\" container ${NTerm}"
+        read -e CONFIRM
+        echo -e "\n"
+
+        if [ "$CONFIRM" = "B" ] || [ "$CONFIRM" = "b" ]; then
+                echo -e "${BRedTerm} To manually build the container issue the following: ${BGreenTerm} docker build -t appcontainers/\"$APP\" . ${NTerm}"
+                echo -e "\n"
+
+                echo -e " ${BYellowTerm} Building \"$APP\" Container... ${NTerm}"
+                docker build -t appcontainers/\"$APP\" .
+                echo -e "${BGreenTerm} [Complete] ${NTerm}"
+                echo -e "\n"
+                sleep 2
+        fi
+
+echo -e "${BYellowTerm} Starting \"$APP\" Container... ${NTerm}"
 docker run -d -i -t \
 --name $RUNNAME \
+-h $RUNNAME \
 -p $PORT:80 \
 -e "APP_NAME=$FQDN" \
 -e "APACHE_SVRALIAS=$FQDN localhost" \
@@ -129,7 +149,7 @@ docker run -d -i -t \
 -e "APP_USER=$USER" \
 -e "APP_PASS=$USERPASS" \
 -e "WP_KEY=$WPKEY" \
-appcontainers/wordpress
+appcontainers/\"$APP\"
 
 echo -e "${BGreenTerm} [Complete] ${NTerm}"
 echo -e "\n"
@@ -159,7 +179,7 @@ echo -e "\n"
 echo -e "\n"
 sleep 1
 
-echo -e "${BYellowTerm} You can now access the Wordpress install by opening a browser and accessing the following IP via a browser ${NTerm}"
+echo -e "${BYellowTerm} You can now access the \"$APP\" install by opening a browser and accessing the following IP via a browser ${NTerm}"
 
 # Display the open URL
 URL="http://$DOCKERIP:$PORT"
@@ -170,6 +190,6 @@ sleep 5
 
 # If on a machine running Boot2Docker then auto open the application page.
 if [[ $B2D != "" ]]
-	then
-	open $URL
+        then
+        open $URL
 fi
